a48b12d0211ebb191790b4b7203531c2c4faf788
CheyanneLee <44128124+Moqixis@users.noreply.github.com>
2022-07-14 11:46:35 +0800

    更新注释

--- models/yolo.py
+++ models/yolo.py
@@ -74,22 +74,36 @@ class Detect(nn.Module):
         for i in range(self.nl): # 对三个feature map分别进行处理
             x[i] = self.m[i](x[i])  # conv  
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+            # 调整tensor的形状
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
+            # self.training属于父类nn.Module的一个变量
+            # model.train()的调用self.training = True; model.eval()的调用self.training = False
             if not self.training:  # inference
                 if self.onnx_dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
 
+                # sigmoid控制数值范围, 对于xywh都做了sigmoid
                 y = x[i].sigmoid()
+                # 选择直接inplace置换,或者再重新拼接输出, 这里是yolov5回归机制的核心代码
                 if self.inplace:
+                    # xy坐标回归预测: bx = 2σ(tx) - 0.5 + cx | by = 2σ(ty) - 0.5 + cy
+                    # box center的x,y的预测被乘以2并减去了0.5，所以这里的值域从yolov3里的(0,1)开区间，变成了(-0.5,1.5)
+                    # 从表面理解是yolov5可以跨半个格点预测了，这样可以提高对格点周围的bbox的召回.
+                    # 还有一个好处就是也解决了yolov3中因为sigmoid开区间而导致中心无法到达边界处的问题
                     y[..., 0:2] = (y[..., 0:2] * 2 + self.grid[i]) * self.stride[i]  # xy
+                    # wh宽高回归预测: bw = pw(2σ(tw))^2 | bh = ph(2σ(th))^2
+                    # 值域从基于anchor宽高的(0, +∞)变成了(0, 4), 预测的框范围更精准了,通过sigmoid约束让回归的框比例尺寸更为合理
                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
                     xy, wh, conf = y.split((2, 2, self.nc + 1), 4)  # y.tensor_split((2, 4, 5), 4)  # torch 1.8.0
+                    # 不同的预测特征层尺度不同, 需要乘上不同的系数回归到原图大小
                     xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
+                    # 不同的预测特征层使用anchor大小不同, 预测目标尺寸不同, 需要乘上相对于特征点的anchor大小
                     wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
+                    # 重新concat拼接在一起
                     y = torch.cat((xy, wh, conf), 4)
-                # z是一个tensor list 三个元素
+                # z是一个tensor list 三个元素 分别是[1, 19200, 25] [1, 4800, 25] [1, 1200, 25]
                 z.append(y.view(bs, -1, self.no))
 
         return x if self.training else (torch.cat(z, 1),) if self.export else (torch.cat(z, 1), x)
@@ -101,12 +115,22 @@ class Detect(nn.Module):
         d = self.anchors[i].device
         t = self.anchors[i].dtype
         shape = 1, self.na, ny, nx, 2  # grid shape
+        # arange(1,10)产生的是10-1=9 由1-9组成的1维度张量 ，类型int
         y, x = torch.arange(ny, device=d, dtype=t), torch.arange(nx, device=d, dtype=t)
+        # torch.meshgrid（）的功能是生成网格，可以用于生成坐标
+        # yv: tensor([[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], ...])
+        # xv: tensor([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], ...])
         if check_version(torch.__version__, '1.10.0'):  # torch>=1.10.0 meshgrid workaround for torch>=0.7 compatibility
             yv, xv = torch.meshgrid(y, x, indexing='ij')
         else:
             yv, xv = torch.meshgrid(y, x)
+        # 构建网格的特征点
+        # torch.stack()在维度上连接（concatenate）若干个张量,tensor.expand()将张量广播到新的形状
+        # (80,80)&(80,80) -> (80,80,2) -> (1,3,80,80,2) 复制3份
         grid = torch.stack((xv, yv), 2).expand(shape) - 0.5  # add grid offset, i.e. y = 2.0 * x - 0.5
+        # 构建网格的anchor, 每一个特征图(h,w)都有3种不同尺度的anchor
+        # tensor.view()方法可以调整tensor的形状，stride是为了复原每个预测特征层的anchor的绝对大小, 因为每一层尺度是不一样的
+        # (3,2) -> (1,3,1,1,2) -> (1,3,80,80,2)
         anchor_grid = (self.anchors[i] * self.stride[i]).view((1, self.na, 1, 1, 2)).expand(shape)
         return grid, anchor_grid
 
@@ -170,7 +194,7 @@ class Model(nn.Module):
         LOGGER.info('')
 
     def forward(self, x, augment=False, profile=False, visualize=False):
-        # augmented inference, None  上下flip/左右flip
+        # augmented inference, None  scale和flip 上下flip/左右flip
         # 是否在测试时也使用数据增强  Test Time Augmentation(TTA)
         if augment:
             return self._forward_augment(x)  # augmented inference, None
@@ -184,14 +208,18 @@ class Model(nn.Module):
         s = [1, 0.83, 0.67]  # scales
         f = [None, 3, None]  # flips (2-ud, 3-lr)
         y = []  # outputs
+        # 这里相当于对输入x进行3次不同参数的测试数据增强推理, 每次的推理结果都保存在列表y中
         for si, fi in zip(s, f):
             # scale_img缩放图片尺寸
+            # 通过普通的双线性插值实现，根据ratio来控制图片的缩放比例，最后通过pad 0补齐到原图的尺寸
             xi = scale_img(x.flip(fi) if fi else x, si, gs=int(self.stride.max()))
             yi = self._forward_once(xi)[0]  # forward
             # cv2.imwrite(f'img_{si}.jpg', 255 * xi[0].cpu().numpy().transpose((1, 2, 0))[:, :, ::-1])  # save
             # _descale_pred将推理结果恢复到相对原图图片尺寸
+            # 如果f=2,进行上下翻转; 如果f=3,进行左右翻转
             yi = self._descale_pred(yi, fi, si, img_size)
             y.append(yi)
+        # 把第一层的后面一部分的预测结果去掉, 也把最后一层的前面一部分的预测结果去掉
         y = self._clip_augmented(y)  # clip augmented tails
         return torch.cat(y, 1), None  # augmented inference, train
 
@@ -206,21 +234,31 @@ class Model(nn.Module):
                            1 一个tensor list 存放三个元素 [bs, anchor_num, grid_w, grid_h, xywh+c+20classes]
                              [1, 3, 80, 80, 25] [1, 3, 40, 40, 25] [1, 3, 20, 20, 25]
         """
+        # y列表用来保存中间特征图; dt用来记录每个模块执行10次的平均时长
         # y: 存放着self.save=True的每一层的输出，因为后面的层结构concat等操作要用到
         # dt: 在profile中做性能评估时使用
         y, dt = [], []  # outputs
+        # 对sequence模型进行遍历操作, 不断地对输入x进行处理, 中间结果需要保存的时候另外存储到列表y中
         for m in self.model:
             # 前向推理每一层结构   m.i=index   m.f=from   m.type=类名   m.np=number of params
+            # 如果只是对前一个模块的输出进行操作, 则需要提取直接保存的中间特征图进行操作,
+            # 一般是concat处理, 对当前层与之前曾进行一个concat再卷积; detect模块也需要提取3个特征层来处理
             if m.f != -1:  # if not from previous layer
                 # 这里需要做4个concat操作和1个Detect操作
                 # concat操作如m.f=[-1, 6] x就有两个元素,一个是上一层的输出,另一个是index=6的层的输出 再送到x=m(x)做concat操作
                 # Detect操作m.f=[17, 20, 23] x有三个元素,分别存放第17层第20层第23层的输出 再送到x=m(x)做Detect的forward
                 x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
             # 打印日志信息  FLOPs time等
+            # profile参数打开会记录每个模块的平均执行10次的时长和flops用于分析模型的瓶颈, 提高模型的执行速度和降低显存占用
             if profile:
                 self._profile_one_layer(m, x, dt)
+            # 使用当前模块对特征图进行处理
+            # 如果是concat模块: 则x是一个特征图列表, 则对其进行拼接处理, 再交给下一个卷积模块;
+            # 如果是C3, Conv等普通的模块: 则x是单一特征图
+            # 如果是detct模块: 则x是3个特征图的列表 (训练与推理返回的内容不一样)
             x = m(x)  # run
             # 存放着self.save的每一层的输出，因为后面需要用来作concat等操作要用到  不在self.save层的输出就为None
+            # self.save: 把所有层结构中from不是-1的值记下并排序 [4, 6, 10, 14, 17, 20, 23]
             y.append(x if m.i in self.save else None)  # save output
             # 特征可视化 可以自己改动想要哪层的特征进行可视化
             if visualize:
@@ -238,9 +276,11 @@ class Model(nn.Module):
         """
         # de-scale predictions following augmented inference (inverse operation)
         if self.inplace:  # 默认执行 不使用AWS Inferentia
-            p[..., :4] /= scale  # de-scale
+            p[..., :4] /= scale  # de-scale  xywh坐标缩放回原来大小
+            # f=2,进行上下翻转
             if flips == 2:
                 p[..., 1] = img_size[0] - p[..., 1]  # de-flip ud
+            # f=3,进行左右翻转
             elif flips == 3:
                 p[..., 0] = img_size[1] - p[..., 0]  # de-flip lr
         else:
@@ -252,30 +292,38 @@ class Model(nn.Module):
             p = torch.cat((x, y, wh, p[..., 4:]), -1)
         return p
 
+    # 这里y的一个包含3个子列表的列表, 通过对输入图像x进行了3次不同尺度的变换, 所以得到了3个inference结构
+    # 这里看不太懂, 不过大概做的事情就是对第一个列表与最后一个列表的结果做一些过滤处理
+    # 把第一层的后面一部分的预测结果去掉, 也把最后一层的前面一部分的预测结果去掉, 然后剩下的concat为一个部分
     def _clip_augmented(self, y):
         # Clip YOLOv5 augmented inference tails
         nl = self.model[-1].nl  # number of detection layers (P3-P5)
         g = sum(4 ** x for x in range(nl))  # grid points
         e = 1  # exclude layer count
-        i = (y[0].shape[1] // g) * sum(4 ** x for x in range(e))  # indices
-        y[0] = y[0][:, :-i]  # large
-        i = (y[-1].shape[1] // g) * sum(4 ** (nl - 1 - x) for x in range(e))  # indices
-        y[-1] = y[-1][:, i:]  # small
+        i = (y[0].shape[1] // g) * sum(4 ** x for x in range(e))  # indices  (25200 // 21) * 1 = 1200
+        y[0] = y[0][:, :-i]  # large  (1,25200,25) -> (1,24000,25)
+        i = (y[-1].shape[1] // g) * sum(4 ** (nl - 1 - x) for x in range(e))  # indices  (12348 // 21) * 16 = 9408
+        y[-1] = y[-1][:, i:]  # small  (1,12348,25) -> (1,2940,25)
         return y
 
     def _profile_one_layer(self, m, x, dt):
         c = isinstance(m, Detect)  # is final layer, copy input as inplace fix
+        # profile函数返回flops与params, [0]表示计算浮点数
         o = thop.profile(m, inputs=(x.copy() if c else x,), verbose=False)[0] / 1E9 * 2 if thop else 0  # FLOPs
         t = time_sync()
+        # 查看模块执行10次的平均时长
         for _ in range(10):
             m(x.copy() if c else x)
         dt.append((time_sync() - t) * 100)
+        # 记录相关参数
         if m == self.model[0]:
             LOGGER.info(f"{'time (ms)':>10s} {'GFLOPs':>10s} {'params':>10s}  {'module'}")
         LOGGER.info(f'{dt[-1]:10.2f} {o:10.2f} {m.np:10.0f}  {m.type}')
+        # 到最后检测头的时候再输出总花费时长
         if c:
             LOGGER.info(f"{sum(dt):10.2f} {'-':>10s} {'-':>10s}  Total")
 
+    # 对Detect()进行初始化
     def _initialize_biases(self, cf=None):  # initialize biases into Detect(), cf is class frequency
         # https://arxiv.org/abs/1708.02002 section 3.3
         # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
@@ -286,6 +334,7 @@ class Model(nn.Module):
             b.data[:, 5:] += math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # cls
             mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
 
+    # 打印模型中最后Detect层的偏置bias信息(也可以任选哪些层bias信息)
     def _print_biases(self):
         """
         打印模型中最后Detect层的偏置bias信息(也可以任选哪些层bias信息)
@@ -301,6 +350,7 @@ class Model(nn.Module):
     #         if type(m) is Bottleneck:
     #             LOGGER.info('%10.3g' % (m.w.detach().sigmoid() * 2))  # shortcut weights
 
+    # 参数重结构化: 融合conv2d + batchnorm2d (推理的时候用, 可以加快模型的推理速度)
     def fuse(self):  # fuse model Conv2d() + BatchNorm2d() layers
         """用在detect.py、val.py
         fuse model Conv2d() + BatchNorm2d() layers
@@ -309,10 +359,14 @@ class Model(nn.Module):
         LOGGER.info('Fusing layers... ')
         # 遍历每一层结构
         for m in self.model.modules():
-            # 如果当前层是卷积层Conv且有bn结构, 那么就调用fuse_conv_and_bn函数讲conv和bn进行融合, 加速推理
+            # 如果当前层是卷积层Conv与DWConv(继承Conv)且有bn结构, 那么就调用fuse_conv_and_bn函数讲conv和bn进行融合, 加速推理
             if isinstance(m, (Conv, DWConv)) and hasattr(m, 'bn'):
+                # 融合Conv模块中的conv与bn层(不包含激活函数), 返回的是参数融合后的卷积
                 m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
+                # 融合后conv的参数就包含了bn的用途, 所以可以删除bn层
                 delattr(m, 'bn')  # remove batchnorm
+                # 由于不需要bn层, 所以forward函数需要改写：
+                # self.act(self.bn(self.conv(x))) -> self.act(self.conv(x))
                 m.forward = m.forward_fuse  # update forward
         self.info()
         return self
